ef2fc104697b31111dfe6a9754dcd6046c9cae7e
roqsanafathima13 <roqsaaan2008@gmail.com>
2026-07-17 11:35:11 +0530

    Implement FooBar class with synchronized printing

---
+++ day4-1115_Print_FooBar_Alternately.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.Semaphore;
+class FooBar {
+    private int n;
+    Semaphore s1 = new Semaphore(0);
+    Semaphore s2 = new Semaphore(1);
+    public FooBar(int n) {
+        this.n = n;
+    }
+    public void foo(Runnable printFoo) throws InterruptedException {   
+        for (int i = 0; i < n; i++) {
+            s2.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            s1.release();
+        }
+    }
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {       
+            s1.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            s2.release();
+        }
+    }
+}
