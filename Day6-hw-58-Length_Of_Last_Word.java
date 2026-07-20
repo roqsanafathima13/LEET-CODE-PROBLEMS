5b8f6259fbe0e398b3c9a4705d48ce4091e057c5
roqsanafathima13 <roqsaaan2008@gmail.com>
2026-07-20 13:43:55 +0530

    Update Day6-hw-58-Length_Of_Last_Word.java

--- Day6-hw-58-Length_Of_Last_Word.java
+++ Day6-hw-58-Length_Of_Last_Word.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim(); // Step 1
-        int lastSpace = s.lastIndexOf(' '); // Step 2
-        return s.length() - lastSpace - 1;  // Step 3
+        s = s.trim(); 
+        int lastSpace = s.lastIndexOf(' '); 
+        return s.length() - lastSpace - 1;  
     }
 }
