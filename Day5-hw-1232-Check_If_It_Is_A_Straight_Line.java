423c29190809902f3271d0ba4b8143777a636d17
roqsanafathima13 <roqsaaan2008@gmail.com>
2026-07-25 20:20:34 +0530

    Update Day5-hw-1232-Check_If_It_Is_A_Straight_Line.java

--- Day5-hw-1232-Check_If_It_Is_A_Straight_Line.java
+++ Day5-hw-1232-Check_If_It_Is_A_Straight_Line.java
@@ -1,19 +1,13 @@
-class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
-        int x0 = coordinates[0][0];
-        int y0 = coordinates[0][1];
-        int x1 = coordinates[1][0];
-        int y1 = coordinates[1][1];
-
-        for (int i = 2; i < coordinates.length; i++) {
-            int x = coordinates[i][0];
-            int y = coordinates[i][1];
-
-            // Cross multiplication to check slope equality
-            if ((y1 - y0) * (x - x1) != (y - y1) * (x1 - x0)) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+class Solution {  
+        public boolean checkStraightLine(int[][] coordinates) {  
+        int x0 = coordinates[0][0], y0 = coordinates[0][1],  
+            x1 = coordinates[1][0], y1 = coordinates[1][1];  
+        int dx = x1 - x0, dy = y1 - y0;  
+        for (int[] co : coordinates) {  
+            int x = co[0], y = co[1];  
+            if (dx * (y - y1) != dy * (x - x1))  
+                return false;  
+        }  
+        return true;  
+    }  
+} 
